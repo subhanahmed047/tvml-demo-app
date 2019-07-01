2169c24d4db17234c4748dfe4020fedf21c8881c
Subhan Ahmed <subhanahmed047@gmail.com>
2019-07-01 11:48:05 +1000

    added a catalog template

--- demo123/application.js
+++ demo123/application.js
@@ -9,58 +9,118 @@
 //
 
 /*
- * This file provides an example skeletal stub for the server-side implementation 
+ * This file provides an example skeletal stub for the server-side implementation
  * of a TVML application.
  *
- * A javascript file such as this should be provided at the tvBootURL that is 
- * configured in the AppDelegate of the TVML application. Note that  the various 
- * javascript functions here are referenced by name in the AppDelegate. This skeletal 
- * implementation shows the basic entry points that you will want to handle 
+ * A javascript file such as this should be provided at the tvBootURL that is
+ * configured in the AppDelegate of the TVML application. Note that  the various
+ * javascript functions here are referenced by name in the AppDelegate. This skeletal
+ * implementation shows the basic entry points that you will want to handle
  * application lifecycle events.
  */
 
 /**
- * @description The onLaunch callback is invoked after the application JavaScript 
- * has been parsed into a JavaScript context. The handler is passed an object 
+ * @description The onLaunch callback is invoked after the application JavaScript
+ * has been parsed into a JavaScript context. The handler is passed an object
  * that contains options passed in for launch. These options are defined in the
  * swift or objective-c client code. Options can be used to communicate to
- * your JavaScript code that data and as well as state information, like if the 
+ * your JavaScript code that data and as well as state information, like if the
  * the app is being launched in the background.
  *
- * The location attribute is automatically added to the object and represents 
+ * The location attribute is automatically added to the object and represents
  * the URL that was used to retrieve the application JavaScript.
  */
 App.onLaunch = (options) => {
-    const alert = createAlert("Hello World!", "Welcome to tvOS");
+    const alert = createAlert('Hello World!', 'Welcome to tvOS');
     navigationDocument.pushDocument(alert);
-}
+};
 
+App.onWillResignActive = () => {};
 
-App.onWillResignActive = () => {
+App.onDidEnterBackground = () => {};
 
-}
+App.onWillEnterForeground = () => {};
 
-App.onDidEnterBackground = () => {
+App.onDidBecomeActive = () => {};
 
-}
-
-App.onWillEnterForeground = () => {
-    
-}
-
-App.onDidBecomeActive = () => {
-    
-}
-
-App.onWillTerminate = () => {
-    
-}
-
+App.onWillTerminate = () => {};
 
 /**
  * This convenience funnction returns an alert template, which can be used to present errors to the user.
  */
 const createAlert = (title, description) => {
+    const catalogDocument = `<?xml version="1.0" encoding="UTF-8" ?>
+<document>
+   <catalogTemplate>
+      <banner>
+         <title>Popular Titles</title>
+      </banner>
+      <list>
+         <section>
+            <listItemLockup>
+               <title>All Titles</title>
+               <decorationLabel>6</decorationLabel>
+               <relatedContent>
+                  <grid>
+                     <section>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 1</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 2</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 3</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 4</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 5</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 6</title>
+                        </lockup>
+                     </section>
+                  </grid>
+               </relatedContent>
+            </listItemLockup>
+            <listItemLockup>
+               <title>Pets</title>
+               <decorationLabel>4</decorationLabel>
+               <relatedContent>
+                  <grid>
+                     <section>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 2</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 1</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 4</title>
+                        </lockup>
+                        <lockup>
+                           <img src="https://loremflickr.com/250/376" width="250" height="376" />
+                           <title>Movie 3</title>
+                        </lockup>
+                     </section>
+                  </grid>
+               </relatedContent>
+            </listItemLockup>
+         </section>
+      </list>
+   </catalogTemplate>
+</document>`;
 
     const alertString = `<?xml version="1.0" encoding="UTF-8" ?>
         <document>
@@ -68,9 +128,13 @@ const createAlert = (title, description) => {
             <title>${title}</title>
             <description>${description}</description>
           </alertTemplate>
-        </document>`
+        </document>`;
 
-    const alertDoc = new DOMParser().parseFromString(alertString, "application/xml");
+    const alertDoc = new DOMParser().parseFromString(
+        // alertString,
+        catalogDocument,
+        'application/xml',
+    );
 
-    return alertDoc
-}
+    return alertDoc;
+};
